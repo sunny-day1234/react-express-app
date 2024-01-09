b90f2b71400f8ce07ee04a2a517062ccdf7dbc25
徐雅君 <jennifer@xuyajuns-MacBook-Air.local>
2024-01-09 21:02:23 +0800

    add something

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [data, setData] = useState([]);
   const getData = async () => {
     const response = await axios.get("/api/users");
+    console.log(123);
     setData(response.data);
   };
 
